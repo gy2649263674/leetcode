9f0985216151460f5e41eb86c626e280be96b879
gy <2649263674@qq.com>
2024-08-18 00:50:17 +0800

    8/18

---
+++ 31.next-permutation.cpp
@@ -0,0 +1,94 @@
+﻿/*
+ * @lc app=leetcode.cn id=31 lang=cpp
+ * @lcpr version=30204
+ *
+ * [31] 涓嬩竴涓帓鍒?
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 1)
+        {
+            return;
+        }
+        int i = n - 2;
+        for (; i >= 0; i--)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                break;
+            }
+            // 1 2 3 4 5
+            // 3 4       5 2 1
+        }
+        if (i>=0)
+        {
+            int ind = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[j] > nums[i])
+                {
+                    // swap(nums[j], nums[i]);
+                    ind = j;
+                }
+                else if (nums[j] < nums[i])
+                {
+                    break;
+                }
+            }
+            swap(nums[i], nums[ind]);
+        }
+        sort(nums.begin() + i + 1, nums.end());
+        return;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,1,5]\n
+// @lcpr case=end
+
+ */
+int main(void)
+{
+    Solution ss;
+    vector<int> vec;
+    ss.nextPermutation(vec = {1,3,2});
+    for (int var : vec)
+    {
+        cout << var << " ";
+    }
+    return 0;
+}
--- 3137.minimum-number-of-operations-to-make-word-k-periodic.cpp
+++ 3137.minimum-number-of-operations-to-make-word-k-periodic.cpp
@@ -1,11 +1,11 @@
-/*
+﻿/*
  * @lc app=leetcode.cn id=3137 lang=cpp
  * @lcpr version=30204
  *
- * [3137] K 周期字符串需要的最少操作次数
+ * [3137] K 鍛ㄦ湡瀛楃涓查渶瑕佺殑鏈€灏戞搷浣滄鏁?
  */
 
-// @lcpr-template-start
+ // @lcpr-template-start
 using namespace std;
 #include <algorithm>
 #include <array>
@@ -22,64 +22,55 @@ using namespace std;
 #include <unordered_set>
 #include <utility>
 #include <vector>
-#include <bits/stdc++.h>
+#include <string.h>
+#include<map>
+//#include <bits/stdc++.h>
 // @lcpr-template-end
 // @lc code=start
 class Solution
 {
 public:
-    int minimumOperationsToMakeKPeriodic(string word, int k)
-    {
-        int ans = 1e9;
-        int len = word.length();
-        for (int i = 0; i +k <= word.length(); i++)
-        {
-            if (i == 0||len % i == 0)
-            {
-                string ss = word.substr(i, k);
-                int tmp =0;
-                for (int j = 0; j+k < len; j += k)
-                {
-                    if (word.substr(j, k) !=ss)
-                    {
-                        ++tmp;
-                    }
-                }
-                ans = min(ans,tmp);
-            }
-        }
-        return ans;
-        // map<string, int> mp;
-        // for (int i = 0; i + k <= word.length(); i++)
-        // {
-        //     if(i== 0 || word.length()%i == 0)
-        //         ++mp[word.substr(i, k)];
-        // }
-        // int ans = 1e9;
-        // for (auto [_, val] : mp)
-        // {
-        //     ans = min(ans, static_cast<int>(word.length() - val*k)/k);
-        // }
-        // return ans;
-    }
+	int minimumOperationsToMakeKPeriodic(string word, int k)
+	{
+		/*int ans = 1e9;
+		int len = word.length();
+		for (int i = 0; i +k <= word.length(); i++)
+		{
+			if (i%k == 0)
+			{
+				string ss = word.substr(i, k);
+				int tmp =0;
+				for (int j = 0; j+k <= len; j += k)
+				{
+					if (word.substr(j, k) !=ss)
+					{
+						++tmp;
+					}
+				}
+				ans = min(ans,tmp);
+			}
+		}
+		return ans;*/
+		map<string, int>mp;
+		int len = word.length();
+		for (int i = 0; i < len; i+=k)
+		{
+			mp[word.substr(i, k)]++;
+		}
+		int N = len / k;
+ 		int ans = 1e9;
+		for (auto &[_,times] : mp)
+		{
+			ans = min(ans, N - times);
+		}
+		return ans;
+	}
 };
-// @lc code=end
 
-/*
-// @lcpr case=start
-// "leetcodeleet"\n4\n
-// @lcpr case=end
-
-// @lcpr case=start
-// "leetcoleet"\n2\n
-// @lcpr case=end
-
- */
-
-// int main(void)
-// {
-//     Solution s;
-//     cout<<s.minimumOperationsToMakeKPeriodic("leetcodeleet", 4);
-//     return 0;
-
-// }
+//int main(void)
+//{
+//	Solution s;
+//	cout << s.minimumOperationsToMakeKPeriodic("aagnnannnnnnnnn", 1);
+//	return 0;
+//
+//}
